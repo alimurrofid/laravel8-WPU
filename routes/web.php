2cee906135b4cd4abbf5f3f01d463ef62b656b39
alimurrofid <alimurrofid77@gmail.com>
2023-04-27 19:22:24 +0700

    membuat Struktur Folder,Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view(
+        'about',
+        [
+            'name' => 'Rofid',
+            'email' => 'rofid@gmail.com',
+            'image' => 'rofid.png'
+        ]
+    );
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
