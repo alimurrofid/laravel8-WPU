2de2aacf3b6c0360255b4d47fecd864f4b53e8e7
alimurrofid <alimurrofid77@gmail.com>
2023-04-29 23:36:56 +0700

    Redesign UI

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+    protected $with = ['category','author'];
 
     //satu post hanya memiliki satu category
     public function category() {
