4bafcd7984db7ad5a9c8fd6f0b6b1794873222d9
alimurrofid <alimurrofid77@gmail.com>
2023-05-02 11:02:03 +0700

    membuat Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = ['id'];
-    protected $with = ['category','author'];
+    protected $with = ['category', 'author'];
 
-    public function scopeFilter($query, array $filters){
+    public function scopeFilter($query, array $filters)
+    {
         // if (isset($filters['search']) ? $filters['search'] : false) {
         //     return $query->where('title', 'like', '%' . $filters['search'] . '%')
         //           ->orWhere('body', 'like', '%' . $filters['search'] . '%');
@@ -20,34 +22,55 @@ class Post extends Model
 
         $query->when($filters['search'] ?? false, function ($query, $search) {
             return $query->where('title', 'like', '%' . $search . '%')
-                  ->orWhere('body', 'like', '%' . $search . '%');
+                ->orWhere('body', 'like', '%' . $search . '%');
         });
 
         $query->when($filters['category'] ?? false, function ($query, $category) {
-            return $query->whereHas('category', function ($query) use ($category){
+            return $query->whereHas('category', function ($query) use ($category) {
                 $query->where('slug', $category);
             });
         });
 
-        $query->when($filters['author'] ?? false, fn($query, $author) =>
-            $query->whereHas('author', fn($query) =>
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
                 $query->where('username', $author)
             )
         );
     }
 
     //satu post hanya memiliki satu category
-    public function category() {
-        return $this->belongsTo( Category::class );
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 
     // satu post hanya memiliki satu user
-    public function author() {
-        return $this->belongsTo( User::class ,'user_id');
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
-   public function getRouteKeyName()
-   {
-       return 'slug';
-   }
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
